16bcca8571ca3713c3b31ffaf7194ef5356b667c
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2025-05-26 19:56:29 +0200

    small check 2

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,8 +1,26 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import { defineProps } from 'vue'
+import { toRefs } from 'vue'
+import { useRouter } from 'vue-router'
+
+import { useGetUserStore } from '@/stores/current-user-store'
+const userStore = useGetUserStore()
+const { user } = toRefs(userStore)
+const props = defineProps(['data'])
+
+const { data } = toRefs(props)
+const router = useRouter()
+const handleSignOut = () => {
+  userStore.signOut(router)
+}
+</script>
+
 <template>
   <div class="w-full flex justify-around bg-green-900 p-2">
     <RouterLink :to="{ name: 'home' }" class="flex items-center justify-center gap-3">
       <div class="flex items-center gap-1">
-        <p class="text-6xl font-stretch-50% text-white">S</p>
+        <p class="text-6xl font-stretch-50% text-white">S 03</p>
 
         <p class="text-4xl font-stretch-50% text-white">'LX</p>
       </div>
@@ -34,23 +52,5 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-import { Icon } from '@iconify/vue'
-import { defineProps } from 'vue'
-import { toRefs } from 'vue'
-import { useRouter } from 'vue-router'
-
-import { useGetUserStore } from '@/stores/current-user-store'
-const userStore = useGetUserStore()
-const { user } = toRefs(userStore)
-const props = defineProps(['data'])
-
-const { data } = toRefs(props)
-const router = useRouter()
-const handleSignOut = () => {
-  userStore.signOut(router)
-}
-</script>
 
 <style scoped></style>
