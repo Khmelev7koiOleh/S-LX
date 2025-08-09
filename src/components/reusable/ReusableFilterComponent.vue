535d06d7368e6f622a64af31b807d7f873df311b
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2025-08-09 19:04:56 +0200

    AdsView.vue has been adapted for the phone mode + reusable filter component has benn added

---
+++ src/components/reusable/ReusableFilterComponent.vue
@@ -0,0 +1,219 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+import { ref, computed, watch, toRefs } from 'vue'
+
+import { defineEmits } from 'vue'
+
+import { adType } from '@/data/ad-type'
+
+import { useWindowSize } from '@/composables/useWindowSize'
+
+const { width, height, isPhone, isTablet, isLaptop } = useWindowSize()
+
+const props = defineProps({
+  items: { type: Array, required: true },
+  searchQuery: { type: String, required: true },
+  selectedCategory: { type: String, required: false },
+})
+const { items } = toRefs(props)
+const emit = defineEmits(['update:searchQuery', 'update:selectedCategory', 'update:filteredItems'])
+
+const searchQuery = computed({
+  get: () => props.searchQuery,
+  set: (val) => emit('update:searchQuery', val),
+})
+
+const selectedCategory = computed({
+  get: () => props.selectedCategory,
+  set: (val) => emit('update:selectedCategory', val),
+})
+
+const onFilterOpen = ref<boolean>(false)
+
+const filterCreatedAt = ref<boolean>(false)
+const filterPrice = ref<boolean>(false)
+const onFilterMenuOpen = ref<boolean>(false)
+const filteredItems = computed(() => {
+  let result = [...items.value]
+  if (searchQuery.value.trim()) {
+    const query = searchQuery.value.toLowerCase()
+    result = result.filter(
+      (ad: any) =>
+        ad.title.toLowerCase().includes(query) || ad.description.toLowerCase().includes(query),
+    )
+  }
+  if (selectedCategory.value) {
+    result = result.filter((ad: any) => ad.type === selectedCategory.value)
+  }
+
+  if (filterCreatedAt.value) {
+    result = result.sort((a, b) => {
+      return new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+    })
+  }
+
+  if (filterPrice.value) {
+    result = result.sort((a, b) => b.price - a.price)
+  }
+
+  return result
+})
+
+const toggleCreatedAtSort = () => {
+  filterCreatedAt.value = !filterCreatedAt.value
+  filterPrice.value = false
+}
+
+const togglePriceSort = () => {
+  filterPrice.value = !filterPrice.value
+  filterCreatedAt.value = false
+}
+
+const filterRemove = () => {
+  console.log(filteredItems.value)
+
+  searchQuery.value = ''
+  selectedCategory.value = ''
+  filterCreatedAt.value = false
+  filterPrice.value = false
+}
+
+watch(searchQuery, (newQuery) => {
+  if (newQuery) {
+  }
+})
+watch(filteredItems, (newVal) => {
+  emit('update:filteredItems', newVal)
+})
+</script>
+
+<template>
+  <div class="bg-gray-50">
+    <!-- <h1 class="text-2xl px-4 py-2">All ads</h1> -->
+    <div class="w-full flex flex-col justify-start items-end gap-2 px-4 py-12">
+      <!-- <div class="w-full flex justify-end">
+      <button class="p-2 bg-fuchsia-500" @click="onFilterOpen = !onFilterOpen">Filter</button>
+    </div> -->
+
+      <div class="w-full flex justify-center items-center gap-2">
+        <div
+          :class="
+            searchQuery?.length
+              ? 'w-[300px]  flex justify-between text-gray-900 placeholder:text-amber-100 px-2 py-2 rounded-sm shadow shadow-blue-400'
+              : 'w-[300px] flex justify-between text-gray-900 placeholder:text-amber-100 px-2 py-2 rounded-sm shadow shadow-gray-400'
+          "
+        >
+          <Icon
+            icon="mdi:magnify"
+            width="24"
+            height="24"
+            :class="searchQuery?.length ? 'text-blue-400' : 'text-gray-900'"
+          />
+          <input
+            placeholder="Search"
+            type="text"
+            v-model="searchQuery"
+            :class="
+              onFilterOpen
+                ? ' focus:outline-none focus:ring-0 '
+                : ' focus:outline-none focus:ring-0 '
+            "
+          />
+        </div>
+        <div
+          :class="
+            isPhone
+              ? 'relative w-[50px] flex items-center justify-center'
+              : 'relative w-fit flex items-center justify-center '
+          "
+        >
+          <button
+            @click="onFilterMenuOpen = !onFilterMenuOpen"
+            v-if="isPhone"
+            class="shadow shadow-gray-400 rounded-full p-2"
+          >
+            <Icon
+              icon="mdi:filter"
+              width="24"
+              height="24"
+              :class="isPhone && onFilterMenuOpen ? ' z-10' : ''"
+            />
+          </button>
+          <div
+            v-if="isLaptop || (isPhone && onFilterMenuOpen)"
+            :class="
+              isPhone && onFilterMenuOpen
+                ? 'absolute top-full center-0  max-w-[100px] flex flex-col justify-center items-center gap-0 bg-gray-100 rounded-sm z-10 m-2  '
+                : 'w-full flex justify-center items-center gap-4'
+            "
+          >
+            <select
+              v-model="selectedCategory"
+              :class="
+                selectedCategory
+                  ? isPhone
+                    ? 'w-[100px] h-[40px] flex justify-center items-center shadow shadow-blue-400 rounded-t-sm text-gray-900  placeholder:text-amber-100 text-center  hover:bg-gray-200 focus:bg-gray-200 cursor-pointer '
+                    : 'w-[250px] h-[40px] flex justify-center items-center shadow shadow-blue-400 text-gray-900 placeholder:text-amber-100 text-center  hover:bg-gray-200 cursor-pointer '
+                  : isPhone
+                    ? 'w-[100px] h-[40px] flex justify-center items-center shadow shadow-gray-400 rounded-t-sm text-gray-900 placeholder:text-amber-10 text-center hover:bg-gray-200 focus:bg-gray-200 cursor-pointer '
+                    : 'w-[250px] h-[40px] flex justify-center items-center shadow  shadow-gray-400 text-gray-900 placeholder:text-amber-100 text-center hover:bg-gray-200 cursor-pointer '
+              "
+            >
+              <option disabled value="">Type</option>
+              <option v-for="type in adType" :key="type.value" :value="type.value">
+                {{ type.title }}
+              </option>
+            </select>
+            <div>
+              <button
+                :class="
+                  filterCreatedAt
+                    ? isPhone
+                      ? 'w-[100px] h-[40px] flex justify-center items-center shadow shadow-blue-400 text-gray-900 placeholder:text-amber-100 text-center rounded-t-sm hover:bg-gray-200 focus:bg-gray-200 cursor-pointer '
+                      : 'w-[250px] h-[40px] flex justify-center items-center shadow shadow-blue-400 text-gray-900 placeholder:text-amber-100 text-center  hover:bg-gray-200 cursor-pointer '
+                    : isPhone
+                      ? 'w-[100px] h-[40px] flex justify-center items-center shadow shadow-gray-400 text-gray-900 placeholder:text-amber-10 text-center hover:bg-gray-200 focus:bg-gray-200 cursor-pointer '
+                      : 'w-[250px] h-[40px] flex justify-center items-center shadow  shadow-gray-400 text-gray-900 placeholder:text-amber-100 text-center hover:bg-gray-200 cursor-pointer '
+                "
+                @click="toggleCreatedAtSort"
+              >
+                Date
+              </button>
+            </div>
+            <div>
+              <button
+                :class="
+                  filterPrice
+                    ? isPhone
+                      ? 'w-[100px] h-[40px] flex justify-center items-center shadow shadow-blue-400 text-gray-900 placeholder:text-amber-100 text-center rounded-t-sm hover:bg-gray-200 focus:bg-gray-200 cursor-pointer '
+                      : 'w-[250px] h-[40px] flex justify-center items-center shadow shadow-blue-400 text-gray-900 placeholder:text-amber-100 text-center  hover:bg-gray-200 cursor-pointer '
+                    : isPhone
+                      ? 'w-[100px] h-[40px] flex justify-center items-center shadow shadow-gray-400 text-gray-900 placeholder:text-amber-10 text-center hover:bg-gray-200 focus:bg-gray-200 cursor-pointer '
+                      : 'w-[250px] h-[40px] flex justify-center items-center shadow  shadow-gray-400 text-gray-900 placeholder:text-amber-100 text-center hover:bg-gray-200 cursor-pointer '
+                "
+                @click="togglePriceSort"
+              >
+                Price
+              </button>
+            </div>
+
+            <div>
+              <button
+                :class="
+                  isPhone
+                    ? 'w-[100px] h-[40px] flex justify-center items-center shadow shadow-gray-400 text-gray-900 text-center placeholder:text-amber-100  focus:bg-gray-200 rounded-b-sm cursor-pointer '
+                    : 'w-[250px] h-[40px] flex justify-center items-center shadow shadow-gray-400 text-gray-900 text-center placeholder:text-amber-100 hover:bg-gray-200  cursor-pointer '
+                "
+                @click="filterRemove"
+              >
+                <!-- <Icon v-if="isPhone" icon="bi:arrow-counterclockwise" width="24" height="24" /> -->
+                <p>Reset</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
